dda5222bb48ef23a38ffe4c8cfd47b800cddc380
Antoine <antoine@MacBook-Pro-de-Antoine.local>
2026-06-07 22:51:20 +0200

    delete unused js file

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,9 @@
 import initTemplateSelect from "./showHideTemplateSelect";
 import initNavPill from "./navPill";
 import initNavBurger from "./navBurger";
-import initNavScrollSpy from "./navScrollSpy";
 
 document.addEventListener("DOMContentLoaded", () => {
     initTemplateSelect();
     initNavPill();
     initNavBurger();
-    initNavScrollSpy();
 });
